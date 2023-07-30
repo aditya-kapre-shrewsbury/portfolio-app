6037d037e51aed262f0ae327e7fa73db61f6e2dd
Aditya <aditya14641@gmail.com>
2023-07-29 21:02:50 -0400

    install react-embed-gist

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,12 +10,22 @@ function App() {
   return (
     <div className="page">
     <div className="flex flex-row overflow-hidden">
-      <div className="flex-none bg-slate-900 h-screen min-w-[25%] fixed text-white justify-center">
-        <div className="nav flex  text-white text-lg mt-10 flex-col align-middle justify-center text-center w-full gap-5 overflow-hidden">
-            <a href="#about">About</a>
-            <a href="#programming">Programming</a>
-            <a href="#devops">DevOps</a>
+      <div className="flex-none bg-slate-900 h-screen min-w-[25%] fixed text-white">
+          <div className="lx ut yr aat adj ajr ark">
+            <nav className="flex text-white text-lg mt-10 flex-col align-middle w-full gap-5 overflow-hidden">
+            <ul className="lx um yr aav m-10">
+              <li className="m-5"><a href="#about">About</a></li>
+                <li className="m-5">
+                  <a href="#programming">Programming</a>
+                  <ul>
+                    <li><a href="#java">Java</a></li>
+                  </ul>
+                </li>
+              <li className="m-5"><a href="#devops">DevOps</a></li>
+            </ul>
+        </nav>
         </div>
+        
         <div className="text-white flex flex-row gap-5 w-fit mx-auto pt-7 aos-init">
           <div onClick={() => window.open("https://github.com/aditya-kapre-shrewsbury", "_blank")}>
             <GitHubIcon className="cursor-pointer hover:scale-105" />
--- src/components/Programming/Java/Java.jsx
+++ src/components/Programming/Java/Java.jsx
@@ -1,8 +1,13 @@
-const Java = () => {
+import ReactEmbedGist from "react-embed-gist";
 
+const Java = () => {
+    
     return (
-        <div>
+        <div id="java">
             Java Tab
+            <ReactEmbedGist gist="aditya-kapre-shrewsbury/6730afaaf36099af4b39d69ba3aa4e1b"
+                wrapperClass="java_gist_wrapper"
+                file=""/>
         </div>
     )
 }
