d3177bbd9cdca3b674a2faf29785861a922275f9
Aditya <aditya14641@gmail.com>
2023-08-04 01:00:52 -0400

    update server to /api/v1

--- server/routes/testAPI.js
+++ server/routes/testAPI.js
@@ -4,4 +4,8 @@ var router = express.Router();
 router.get("/", function(req, res, next) {
     res.send("API is working properly");
 });
+
+router.get("/say", function (req, res, next) {
+    res.send("Hello world!");
+});
 module.exports = router;
